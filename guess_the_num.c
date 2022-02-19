89baed6d8c984f6023e2b17ac7edfe84bd64413f
Shivam Raj <83547835+Shivamr1918@users.noreply.github.com>
2022-02-19 21:11:08 +0530

    C

---
+++ guess_the_num.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+int ci;
+int ui;
+int gss = 0;
+
+srand(time(0));
+ci = rand()%100 + 1;
+printf("%d", ci);
+while (ui!=ci)
+{
+    printf("\nguess the number: ");
+    scanf("%d", &ui);
+    gss++;
+
+    if (ui == ci)               
+    {
+        printf("You guess it right");
+    }
+    else if (ci > ui)
+    {
+        printf("Choose big num");
+    }
+    else if (ci<ui)
+    {
+        printf("Choose small num");
+    } 
+
+}
+
+printf("  you guess it in %d times", gss);
+
+return 0;
+}
