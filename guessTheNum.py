659433afcad288beeeea1b9ea1b20931d114168a
Shivam Raj <83547835+Shivamr1918@users.noreply.github.com>
2022-02-20 14:41:00 +0530

    Using Python

--- guessTheNum.py
+++ guessTheNum.py
@@ -1,24 +1,42 @@
-import random 
-randNumber = random.randint(1, 100)
+import random  # import random
+
+# random number between 1 to 100
+rd = random.randint(1, 100)
+
+# user guesses none mean nothing
 userGuess = None
-guesses = 0
 
-while(userGuess != randNumber):
-    userGuess = int(input("Enter your guess: "))
-    guesses += 1
-    if(userGuess==randNumber):
-        print("You guessed it right!")
+# how many time to guess it
+guess = 0
+
+# when you guess it right its return false
+while userGuess != rd:
+
+    # For user input in int
+    userGuess = int(input("Guess the number: "))
+
+    # guesses +1 mean when while run he add 1 in guess variable
+    guess += 1
+
+    # Conditional to cheak
+    if rd == userGuess:
+        print("you guess it write \n")
+    elif rd > userGuess:
+        print("Choose! large num  ")
     else:
-        if(userGuess>randNumber):
-            print("You guessed it wrong! Enter a smaller number")
-        else:
-            print("You guessed it wrong! Enter a larger number")
+        print("Choose! Small num  ")
 
-print(f"You guessed the number in {guesses} guesses")
-with open("hiscore.txt", "r") as f:
-    hiscore = int(f.read())
+# Printing how many guess to guess it right
+print(f"it was {rd} and you guess it in {guess} times")
 
-if(guesses<hiscore):
-    print("You have just broken the high score!")
-    with open("hiscore.txt", "w") as f:
-        f.write(str(guesses))
+
+'''For store  hi score '''
+# # Open hiscore.txt for store hi score
+# with open("log\hiscore.txt", "r")as f:
+#     fileread = int(f.read())
+
+# # To store hi score
+# if guess < fileread:
+#     print("you have just! broken hi score")
+#     with open("log\hiscore.txt", "w")as f:
+#         f.write(str(guess))
