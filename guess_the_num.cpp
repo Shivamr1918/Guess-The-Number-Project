43a262dd1f37bb47c4f06dbbb948d123cc157955
Shivam Raj <83547835+Shivamr1918@users.noreply.github.com>
2022-02-20 14:43:06 +0530

    Using C++

--- guess_the_num.cpp
+++ guess_the_num.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
-int main(){
+
+int main()
+{
     int ci;
     int ui;
     int gss = 0;
     srand((unsigned)time(0));
-    ci = (rand()%100)+1; 
-    cout << ci << "\n"; 
-    
-    while (ui!=ci)
-{
-    printf("\nguess the number: ");
-    scanf("%d", &ui);
-    gss++;
+    ci = (rand() % 100) + 1;
+    // cout << ci << "\n";
 
-    if (ui == ci)               
+    while (ui != ci)
     {
-        printf("You guess it right");
+        cout << "\nguess the number: ";
+        cin >>ui;
+        gss++;
+
+        if (ui == ci)
+        {
+            cout << "You guess it right";
+        }
+        else if (ci > ui)
+        {
+            cout << "Choose big num";
+        }
+        else if (ci < ui)
+        {
+            cout << " Choose small num";
+        }
+       
     }
-    else if (ci > ui)
-    {
-        printf("Choose big num");
-    }
-    else if (ci<ui)
-    {
-        printf("Choose small num");
-    } 
-printf("  you guess it in %d times", gss);
-return 0;
-}
+    cout << " you guess it in " << gss <<" times" ;
+    return 0;
 }
